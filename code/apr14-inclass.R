3584dc01b5f1669f23ee3b8683296679b5c0ade6
Haley Jeppson <hjeppson@iastate.edu>
2020-04-21 09:03:37 -0500

    update sched

---
+++ code/apr14-inclass.R
@@ -0,0 +1,143 @@
+# class 4/2/20
+
+# slides: maps ---------------------
+library(tidyverse)
+states <- map_data("state")
+head(states)
+
+tail(states)
+
+ggplot(states, aes(x = long, y = lat)) + geom_point()
+
+ggplot(states, aes(x = long, y = lat)) + geom_line()
+
+ggplot(states, aes(x = long, y = lat)) + geom_line(aes(group = group))
+
+ggplot(states, aes(x = long, y = lat)) + geom_path()
+
+ggplot(states, aes(x = long, y = lat)) + geom_path(aes(group = group))
+
+
+## YOUR TURN
+
+# Use ggplot2 and pull out map data for all US counties: 
+  
+counties <- map_data("county")
+
+# Draw a map of counties (polygons & path geom)
+
+ggplot(data = counties, aes(x = long, y = lat)) + geom_path()
+
+ggplot(data = counties, aes(x = long, y = lat)) + 
+  geom_path(aes(group = group))
+
+ggplot(data = counties, aes(x = long, y = lat)) +
+  geom_polygon(aes(group = group), color = "white")
+
+ggplot(data = counties, aes(x = long, y = lat)) +
+  geom_polygon(aes(group = group), color = "white", size = 0.1)
+
+ggplot(data = counties, aes(x = long, y = lat)) +
+  geom_polygon(aes(group = group), color = "white", size = 0.1, fill = "yellowgreen")
+
+ggplot(data = counties, aes(x = long, y = lat)) +
+  geom_polygon(aes(group = group), color = "black", size = 0.1, fill = "yellowgreen")
+
+ggplot(data = counties, aes(x = long, y = lat)) + 
+  geom_polygon(aes(group = group), color = "black", size = 0.1, fill = "yellowgreen") +
+  theme_void()
+
+ggplot(data = counties, aes(x = long, y = lat)) + 
+  geom_polygon(aes(group = group, fill = region), color = "white", size = 0.1, show.legend = FALSE)
+
+# Colour all counties called "story"
+
+counties %>% 
+  mutate(filled = subregion == "story") %>% 
+  ggplot(aes(x = long, y = lat)) + 
+  geom_polygon(aes(group = group, fill = filled), color = "white", size = 0.1)
+
+counties %>% 
+  ggplot(aes(x = long, y = lat)) + 
+  geom_polygon(aes(group = group, fill = subregion == "story"), color = "white", size = 0.1)
+
+
+# What county names are used most often?
+
+counties %>% 
+  count(subregion) %>% 
+  arrange(desc(n))
+
+head(counties)
+
+
+counties %>% 
+  distinct(region, subregion) %>% 
+  count(subregion) %>% 
+  arrange(desc(n))
+
+counties %>% 
+  mutate(jeff_wash = replace(subregion, !(subregion %in% c("jefferson", "washington")), NA)) %>% 
+  ggplot(aes(x = long, y = lat)) + 
+  geom_polygon(aes(group = group, fill = jeff_wash), color = "white", size = 0.1)
+
+
+#----------------------
+# geom_sf()
+
+library(sf)
+states <- USAboundaries::us_boundaries()
+states %>% select(name, geometry) %>% slice(1:10)
+head(states)
+
+states48 <- states %>% 
+  dplyr::filter(stringr::str_detect(name, "Hawaii|Alaska|Puerto", negate = T))
+
+ggplot() + geom_sf(data = states48, aes(geometry = geometry))
+
+
+ggplot() + geom_sf(data = states48)
+
+ggplot() + geom_sf(data = states48, aes(fill = name), show.legend = FALSE)
+
+
+ggplot() + geom_sf(data = states48, aes(fill = awater))
+
+## YOUR TURN 
+
+# devtools::install_github("hrbrmstr/albersusa")
+library(albersusa)
+states <- usa_sf()
+
+# Use geom_sf() and the states data to create a map of the US.
+ggplot(states) + geom_sf()
+
+#-----------------------------------
+data(fbi, package="classdata")
+fbi14 <- fbi %>% dplyr::filter(Year == 2014)
+head(fbi14)
+
+states <- map_data("state")
+head(states)
+
+fbi14$region <- tolower(fbi14$State)
+head(fbi14)
+
+nomatch1 <- fbi14 %>% anti_join(states, by="region")
+unique(nomatch1$State)
+
+nomatch2 <- states %>% anti_join(fbi14, by="region")
+
+unique(nomatch2$State)
+
+fbi.map <- fbi14 %>% left_join(states, by="region")
+
+fbi.map %>% 
+  dplyr::filter(Type=="Burglary") %>% 
+  mutate(Rate = Count/Population) %>% 
+  ggplot(aes(x = long, y = lat, fill=Rate)) +
+  geom_polygon(aes(group=group))
+
+
+
+
---
+++ code/apr16-inclass.R
@@ -0,0 +1,114 @@
+# class 4/2/20
+
+# slides: maps ---------------------
+library(tidyverse)
+
+data(fbi, package="classdata")
+ 
+
+states <- map_data("state")
+head(states)
+
+fbi %>% 
+  filter(Year == 2014) %>% 
+  mutate(region = tolower(State),
+         rate = Count/Population) %>% 
+  left_join(states, by="region") %>% 
+  filter(Type=="Burglary") %>% 
+  ggplot(aes(x = long, y = lat, fill= rate)) +
+  geom_polygon(aes(group=group), color = "white", size = 0.2)
+
+## YOUR TURN
+
+#Draw a choropleth map of the rate of motor vehicle thefts in 2012 across the US.
+
+# unique(fbi$Type)
+
+car_map <- fbi %>% 
+  filter(Year == 2012) %>% 
+  mutate(region = tolower(State),
+         rate = Count/Population*60000,
+         rate = ifelse(State == "Iowa", NA, rate)) %>% 
+  left_join(states, by="region") %>% 
+  filter(Type=="Motor.vehicle.theft") %>% 
+  ggplot(aes(x = long, y = lat, fill= rate)) +
+  geom_polygon(aes(group=group), color = "white", size = 0.2)
+
+car_map
+# scale_fill_gradient2() allows you to set a color scheme with two main colors. 
+# Read up on it and change the scheme in the first choropleth map.
+
+?scale_fill_gradient2
+
+car_map + 
+  scale_fill_gradient2(midpoint = 200, na.value = "grey80", low = "#2874A6", high = "#E74C3C") + 
+  theme_bw()
+
+car_map + 
+  scale_fill_gradient2(midpoint = 200, na.value = "grey80", low = "#28B463", high = "#F39C12") + 
+  theme_bw()
+
+# -------------------------
+## YOUR TURN
+acc <- read.csv("https://raw.githubusercontent.com/DS202-at-ISU/labs/master/data/fars2016/accident.csv", stringsAsFactors = FALSE)
+names(acc)
+head(acc)
+
+# Use the accident data to plot the geographic location of all accidents in the US in 2016.
+
+#unique(acc$YEAR)
+# map %>% ggplot(aes(x = long, y = lat)) + 
+#   geom_polygon(aes(group = group)) +
+#   geom_point(data = content, aes(x=longitude, y = latitude))
+
+# states <- map_data("state")
+
+states %>% 
+  ggplot(aes(x = long, y = lat)) +
+  geom_path(aes(group = group)) 
+
+states %>% 
+  ggplot(aes(x = long, y = lat)) +
+  geom_path(aes(group = group)) + 
+  geom_point(data = acc, aes(x = LONGITUD, y = LATITUDE))
+
+states %>% 
+  ggplot(aes(x = long, y = lat)) +
+  geom_path(aes(group = group)) + 
+  geom_point(data = acc %>% filter(LONGITUD < 500, LONGITUD > -130), aes(x = LONGITUD, y = LATITUDE))
+
+
+states %>% 
+  ggplot(aes(x = long, y = lat)) +
+  geom_path(aes(group = group)) + 
+  geom_point(data = acc %>% filter(LONGITUD < 500, LONGITUD > -130), 
+             aes(x = LONGITUD, y = LATITUDE), size = 0.5)
+
+
+states %>% 
+  ggplot(aes(x = long, y = lat)) +
+  geom_polygon(aes(group = group), fill = "white", colour = "black", size = 0.5) + 
+  geom_point(data = acc %>% filter(LONGITUD < 500, LONGITUD > -130), 
+             aes(x = LONGITUD, y = LATITUDE), size = 0.5,
+             color = "violetred4", alpha = 0.4)
+
+states %>% 
+  ggplot(aes(x = long, y = lat)) +
+  geom_path(aes(group = group)) + 
+  geom_point(data = acc %>% filter(LONGITUD < 500, LONGITUD > -130), 
+             aes(x = LONGITUD, y = LATITUDE), size = 0.1,
+             color = "violetred4", alpha = 0.1)
+
+acc
+# Plot accidents on a map of the US (use the map of the US as first layer)
+
+
+
+
+# Why would it be tricky to plot a choropleth map of the number of accidents by state?
+
+
+library(albersusa)
+states_sf <- usa_sf()
+head(states_sf)
+
